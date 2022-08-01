065c3f43dc9dede656b9dab8f7e1fc1b327f78cb
Atul Kr Karn <atul48kumar90@gmail.com>
2022-08-01 21:01:27 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import App from './App';
+
+const Root = ReactDom.createRoot(document.getElementById('root'));
+
+root.render(
+    <App />
+)
