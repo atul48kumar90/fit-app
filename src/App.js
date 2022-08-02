77417fdb86eab5c2678278524e7cd0ea94a8e22e
Atul Kr Karn <atul48kumar90@gmail.com>
2022-08-02 18:14:28 +0530

    navBar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
+import { Box } from '@mui/material';
+import ExerciseDetails from './pages/ExerciseDetails';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 const App = () => {
-    return {
-        <div> App </div>
-    }
+    return (
+        <Box width = '400px' sx = {{ width: { xl: '1488px'}}} m ="auto">
+            <Navbar />
+                <Routes>
+                    <Route path = "/" element = {<Home />} />
+                    <Route path = "exercise/:id" element = {<ExerciseDetails />} />
+                </Routes>
+            <Footer />
+        </Box>
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-const Root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(
+<BrowserRouter>
+<App />
+</BrowserRouter>
+);
 
 
