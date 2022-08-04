dfd294533544a4c4c42196076b4ef6176e1dd264
Atul Kr Karn <atul48kumar90@gmail.com>
2022-08-04 20:43:07 +0530

    not in mood

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Pagination from '@mui/material/Pagination';
 
-const Exercises = () => {
+import {exerciseOptions, fetchData } from '../utils/fetchData';
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
     return(
         <div>
             Exercises
